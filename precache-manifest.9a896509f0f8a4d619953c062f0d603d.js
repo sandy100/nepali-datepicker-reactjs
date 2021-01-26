6422ea2647fe9cae68b887837bdc3407bf4cd44f
Sandeep <sandeep.bhatt59@gmail.com>
2021-01-26 12:20:51 +0545

    Updates

---
+++ precache-manifest.9a896509f0f8a4d619953c062f0d603d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798a6956dfef0251615a60cedb772493",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a375bb1abd17d8e39a1c",
+    "url": "./static/css/main.336a02ce.chunk.css"
+  },
+  {
+    "revision": "11480f791471857e03d8",
+    "url": "./static/js/2.b068fd7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b068fd7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a375bb1abd17d8e39a1c",
+    "url": "./static/js/main.efc499eb.chunk.js"
+  },
+  {
+    "revision": "b213050908112a8cdfd0",
+    "url": "./static/js/runtime-main.e5edc7d0.js"
+  },
+  {
+    "revision": "d97d73084b4ccb15bcd18c9fdc7ce11a",
+    "url": "./static/media/logo.d97d7308.svg"
+  }
+]);
